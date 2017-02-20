48a58ea5b9a897bbd3b32641a9b6241508090ca4
Sidnev <alexey.sidnev@intel.com>
2017-02-20 13:18:12 +0300

    TreeFind

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,42 @@
 #include <gtest/gtest.h>
-#include "add.h"
+#include "treefind.h"
 
-TEST(Addition, CanAddTwoNumbers) {
-  EXPECT_EQ(add(2, 2), 4);
-  EXPECT_EQ(add(-2, 2), 0);
+
+TEST(Find, FindDontChangeTree) {
+  CNode* t = CreateTree();
+
+  find(&t, 5);
+
+  ASSERT_EQ(t->val, 4);
+  ASSERT_EQ(t->left->val, 2);
+  ASSERT_EQ(t->right->val, 6);
+  ASSERT_EQ(t->right->left->val, 5);
+  ASSERT_EQ(t->right->right->val, 7);
+  ASSERT_EQ(t->left->left->val, 1);
+  ASSERT_EQ(t->left->right->val, 3);
+}
+
+TEST(Find, CanFind) {
+  CNode* t = CreateTree();
+
+  CNode* n = *(find(&t, 5));
+
+  ASSERT_EQ(n->val, 5);
+}
+
+TEST(Find, FindReturnRightPosition) {
+  CNode* t = CreateTree();
+
+  CNode** n = find(&t, 8);
+  CNode* nl = 0;
+
+  ASSERT_EQ(*n, nl);
+
+  *n = new CNode(8);
+
+  CNode* nw = *(find(&t, 8));
+
+  ASSERT_EQ(nw->val, 8);
 }
 
 int main(int ac, char* av[]) {
