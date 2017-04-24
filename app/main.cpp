0e504d962d7ca27614567f82c758bef78af042a4
ZinovievVladimir <zinovievvladimir97@gmail.com>
2017-04-24 16:10:49 +0300

    class

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,61 @@
-#include "add.h"
+#include "UF.h"
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#define n 6
+#define m 6
 
 int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+  bool** island = new bool*[n];
+  for (int i = 0; i < n; i++)
+    island[i] = new bool[m];
+
+  srand(time(0));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (rand() % 2 == 0) {
+        island[i][j] = false;
+      }
+      else {
+        island[i][j] = true;
+      }
+      std::cout << island[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  isl(island, n, m);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      std::cout << island[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (rand() % 2 == 0) {
+        island[i][j] = false;
+      }
+      else {
+        island[i][j] = true;
+      }
+      std::cout << island[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+
+  isl_sh(island, n, m);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      std::cout << island[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
 }
---
+++ include/UF.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_ADD_H_
+#define INCLUDE_ADD_H_
+
+int Find(int* mas, int pos);
+
+void Union(int* mas, int* len, int a, int b);
+
+void isl(bool** arr, int n, int m);
+void isl_sh(bool** arr, int n, int m);
+
+#endif  // INCLUDE_ADD_H_
