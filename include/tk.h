fd497a56a28cd472f62e3be2e68b9ecd2d1dc97b
ZinovievVladimir <zinovievvladimir97@gmail.com>
2017-05-15 19:04:45 +0300

    tk15.05.17

---
+++ include/tk.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_TK_H_
+#define INCLUDE_TK_H_
+#include <vector>
+
+using std::pair;
+using std::vector;
+
+vector< int > tk(vector <pair <int, int> >, int);
+
+#endif  // INCLUDE_TK_H_
